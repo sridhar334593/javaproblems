0e89769ebeb3f3165094e76ed7c32c5caeb47879
sridhar <sridharahdirs33@gmail.com>
2026-07-05 22:08:43 +0530

    CountNums

---
+++ src/com/sri/CountNums.java
@@ -0,0 +1,18 @@
+package com.sri;
+
+public class CountNums {
+    public static void main(String[] args) {
+        int n = 45536;
+        int count = 0;
+        while(n > 0){
+            int rem = n % 10;
+            if (rem == 5){
+                count++;
+
+            }
+            n = n / 10;
+
+        }
+        System.out.println(count);
+    }
+}
